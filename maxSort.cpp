b03dc59a9c609b3a8c773ad031bb1bf6c0bb077f
ryanphadnis <phadrc18@wfu.edu>
2019-09-11 20:12:23 -0400

    Sorted the vector in ascending order

--- maxSort.cpp
+++ maxSort.cpp
@@ -4,27 +4,37 @@ using namespace std;
 
 // function declarations
 void maxSort(vector<int>& list);
-void swap(int& a, int& b);
+void swap(vector<int> &v, int a, int b);
 void print(const vector<int>& list);
 
 int main() {
 
     vector<int> v(5);  // vector to be sorted
     // initialize v
-    /* your code here */
-
+    cout << "Type 5 numbers you want to sort: " << endl;
+    for(int i = 0; i < 5; i++)
+    {
+        cin >> v.at(i);
+    }
 
     // print the unsorted v
-    /* your code here */
+    cout << "This is the unsorted vector: ";
+    for(int i = 0; i < v.size(); i++)
+    {
+        cout << v.at(i) << " ";
+    }
+    cout << endl;
 
 
     // sort v
-    /* your code here */
-
+    maxSort(v);
 
     // print the sorted v
-    /* your code here */
-
+    cout << "This is the sorted vector: ";
+    for(int i = 0; i < v.size(); i++) {
+        cout << v.at(i) << " ";
+    }
+    cout << endl;
 
     return 0;
 }
@@ -35,22 +45,34 @@ int main() {
 void maxSort(vector<int>& list){
     for(int i=list.size()-1;i>=0;i--){
         /*complete the code*/
-
-        for(int j=0;j<=i;j++){
-            /*complete the code*/
-
+        int maxVal = list.at(0);
+        int maxValIndex=0;
+        for(int j=0;j<=i;j++) {
+            if (list.at(j) > maxVal) {
+                maxVal = list.at(j);
+                maxValIndex=j;
+            }
         }
-        /*complete the code*/
+    //complete code here
+        swap(list,maxValIndex, i);
     }
 }
 
 // swap function
-void swap(int& a, int&b){
-    /*your code here*/
+void swap(vector<int> &whatever, int a, int last){
+    int temp1;
+    temp1 = whatever.at(a);
+    whatever.at(a)=whatever.at(last) ;
+    whatever.at(last)=temp1;
+
 }
 
 // print function
 void print(const vector<int>& list){
-    /*your code here*/
+    for(int i = 0; i < list.size() - 1; i++)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
 }
 
